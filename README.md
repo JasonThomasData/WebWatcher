707599b9284cfdd73aff06672a633c313db16557
JasonThomasData <jason.robert.thomas.123@gmail.com>
2015-10-11 21:40:36 +1100

    Sunday, Oct 11

--- README.md
+++ README.md
@@ -9,32 +9,34 @@ This script will:
 - Emails you with a list of sites that have changed.
 
 This is obviously a time saver, and valuable if you work in news.
-I suggest people should not overdo this - don't go crazy and scrape a site every 5 minutes.
+I suggest people should not overdo this - don't go crazy and scrape a site every 5 minutes please.
 I've got my one running on a virtual private server.
 It uses Cron - a scheduler for Linux distros - to run this job every two hours.
 
 If you've also got a Linux-based VPS, and you've set up an SSH key with the server (you really should get a SSH key):
-- Upload the files to your directory using sftp, for me, ```sftp userName@serverIpAddress```
-- Log into the server using ssh, for me this is ```ssh userName@serverIpAddress```
+- Upload files with sftp.
+- Logout, login with ssh to the server.
 - Change the timezone on the host machine, via terminal ```dpkg-reconfigure tzdata```
 - Restart cron ```sudo service cron restart```
 - Load crontab ```crontab -e```
 - Cron has an explainer about how to format your jobs, follow that.
 
-You'll notice all file locations in this script are referennced from the server's root folder.
-You'll need to change the file locations inside the python script, to the same file path that Cron uses, where mine say ```/home/userName/python/webWatcher/```
+You'll notice all file locations in this script are referenced from the server's root folder.
+You'll need to change the file locations when you call the _init_ function, to the same file path that Cron uses, where mine say ```/home/userName/python/webWatcher/```
 
 Your CSV takes:
-- Every row as a website's to scrape
-- Every row has three cells
-- The first cell is the URL of the site to scrape
-- The second cell is the element identifier to look for, eg, the ID or the class.
-- The third cell is the name of the element's class or ID
-- If you just want to scrape the whole page, enter null in the second and third cells for each row. Then the scraper will look for the html's body
+- Every row as a website to scrape.
+- Every row has four cells.
+- The first cell is the URL of the site to scrape.
+- The second cell is the kind of element to look for, eg, div or ul.
+- The third cell is the element identifier to look for, eg, the ID or the class. 
+- The fourth is the name of the element's class or ID.
+- Be careful not to have spaces in the columns if they aren't intended to be there, or BeautifulSoup can't find the element.
+- If you just want to scrape the whole page, enter null in the second and third cells for each row. Then the scraper will look for the html's body.
 
 Hope this script saves you some sanity.
 
 TO DO
-- Fix the bug that says some websites couldn't scrape, then saves them. I'm catching an error in the wrong spot.
-- Fix the bug that won't save sites (possibly same bug)
-- Add a function to push the results to an SQL database, and use Flask to create html templates from those results. So, lists of people can choose to receive emails or check a website, or both.
+- Add a virtual env to emulate javascript for dynamic pages
+- Add a function to get the string that wasn't there in the older html, and tell the user what that is, so users don't have to follow links.
+- Add a function to push the results to an SQL database, and use Flask to create html templates from those results. So, users can choose to receive emails or check a website, or both.
---
+++ WebWatcher.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+import requests, re, csv, sys, smtplib
+from bs4 import BeautifulSoup
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from datetime import datetime
+
+initTime = datetime.now()
+
+def send_email(parseToEmail, toAddr, fromAddr, fromPswd):
+    #Send yourself the results. Need to setup an email account.
+    msg = MIMEMultipart()
+    msg['From'] = fromAddr
+    msg['To'] = toAddr
+    msg['Subject'] = 'Websites, update @ %s' %(str(initTime)[:16])  #Cut off the decimal places from the local time
+    msg.attach(MIMEText(parseToEmail, 'plain'))
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    server.starttls()
+    server.login(fromAddr, fromPswd)
+    text = msg.as_string()
+    server.sendmail(fromAddr, toAddr, text)
+    server.quit()
+    return
+
+def format_email_text(websiteNotFound, pagesChanged, couldNotFind, contentMissing):
+    if len(pagesChanged) > 0:
+        pagesChangedString = '\n'.join(pagesChanged)
+    else:
+        pagesChangedString = 'none'
+    pagesChangedParse = 'These pages changed: \n%s\n' % (pagesChangedString)
+    if len(websiteNotFound) > 0:    
+        websiteNotFoundString = '\n'.join(websiteNotFound)
+    else:
+        websiteNotFoundString = 'none'
+    websiteNotFoundParse = 'Could not be found online: \n%s\n' % (websiteNotFoundString)
+    if len(contentMissing) > 0:
+        contentMissingString = '\n'.join(contentMissing)
+    else:
+        contentMissingString = 'none'        
+    contentMissingParse = 'Could not find content: \n%s\n' % (contentMissingString)
+    if len(couldNotFind) > 0:
+        couldNotFindString = '\n'.join(couldNotFind)
+    else:
+        couldNotFindString = 'none'    
+    couldNotFindParse = 'Scraped and saved for the first time: \n%s\n' % (couldNotFindString)
+    finTime = datetime.now()
+    timeTaken = finTime - initTime
+    timeString = 'The scraping took %s\n ~Started at %s and finished at %s' %(timeTaken, initTime, finTime)
+    return '%s\n\n%s%s%s\n%s' % (pagesChangedParse, websiteNotFoundParse, contentMissingParse, couldNotFindParse, timeString)
+    
+def get_markup(row, websiteNotFound, contentMissing):
+    #Scrapes the page. If no such page, or if page has no content, this pushes errors to arrays for email
+    print row[0]
+    try:
+        urlHeaders = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1 (.NET CLR 3.5.30729)", "Referer": "http://example.com"}
+        r = requests.get(row[0], headers=urlHeaders, timeout=None)
+    except IOError:
+        websiteNotFound.append(row[0])
+        print 'Site missing'
+        return ''
+    try:
+        soup = BeautifulSoup(r.content, 'html5lib')
+        if row[2] == 'null' and row[3] == 'null':
+            findHtml = soup.find('body')
+            findHtml = str(findHtml.findAll(text=True))
+        else:
+            findHtml = soup.find(row[1], {row[2] : row[3]})
+            findHtml = str(findHtml.findAll(text=True))
+        #return findHtml.encode('ascii')
+        return re.sub('[\n\t\s\d\W]', '', findHtml)
+    except AttributeError:
+        contentMissing.append(row[0])
+        print 'No content'
+        return ''
+
+def _init_(filePathForCron, fileWithURLs, toAddr, fromAddr, fromPswd):
+    #Start looping through the CSV, comparing and saving markup
+    pagesChanged = []
+    websiteNotFound = []
+    couldNotFind = []
+    contentMissing =[]
+    with open(filePathForCron + fileWithURLs, 'r') as csvFile:
+        csvRead = csv.reader(csvFile, delimiter=',')
+        for row in csvRead:
+            externalHtml = get_markup(row, websiteNotFound, contentMissing)
+            fileName = re.sub(r'[^\w]', '', row[0])
+            fileName = fileName[:75] #Watch this, if you're scraping sites with the same URL, eg different q strings, you'll need to make this longer
+            try:
+                with open(filePathForCron + 'savedFiles/' + fileName + '.txt', 'r') as internalHtml:
+                    if externalHtml != internalHtml.read():
+                        pagesChanged.append(row[0])
+            except IOError:
+                couldNotFind.append(row[0])
+            with open(filePathForCron + 'savedFiles/' + fileName + '.txt', 'w') as fileToSave:
+                fileToSave.write(externalHtml)
+    parseToEmail = format_email_text(websiteNotFound, pagesChanged, couldNotFind, contentMissing)
+    send_email(parseToEmail, toAddr, fromAddr, fromPswd)
+
+_init_('/path/to/scraper/', 'websitesToScrape.csv', 'RECIPIENT_EMAIL_ADDRESS', 'SENDER_EMAIL_ADDRESS', 'SENDER_EMAIL_PASSPHRASE')
+sys.exit()
