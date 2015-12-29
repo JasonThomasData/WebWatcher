c38f732dc514484647562959ca17e4d132e2e981
JasonThomasData <jason.robert.thomas.123@gmail.com>
2015-12-29 23:27:05 +1100

    Tues, December 29

--- README.md
+++ README.md
@@ -4,39 +4,28 @@ A web scraper for keeping an eye on websites. Some time ago I got sick of checki
 This script will:
 - Take a CSV file with a list of websites.
 - Scrape the markup for each and compare that to the previous markup for that site (savedFiles folder).
+- Take a screenshot you can compare in the future, if you're not sure a site changed.
 - If the markup is different, the script pushes this URL to a list of changed URLs.
 - Saves the new markup.
 - Emails you with a list of sites that have changed.
+- Places all details in an html page for you to look at.
 
-This is obviously a time saver, and valuable if you work in news.
-I suggest people should not overdo this - don't go crazy and scrape a site every 5 minutes please.
 I've got my one running on a virtual private server.
 It uses Cron - a scheduler for Linux distros - to run this job every two hours.
 
 If you've also got a Linux-based VPS, and you've set up an SSH key with the server (you really should get a SSH key):
 - Upload files with sftp.
 - Logout, login with ssh to the server.
-- Change the timezone on the host machine, via terminal ```dpkg-reconfigure tzdata```
+- Change the timezone on the host machine, via terminal ```sudo dpkg-reconfigure tzdata```
 - Restart cron ```sudo service cron restart```
 - Load crontab ```crontab -e```
 - Cron has an explainer about how to format your jobs, follow that.
 
 You'll notice all file locations in this script are referenced from the server's root folder.
-You'll need to change the file locations when you call the _init_ function, to the same file path that Cron uses, where mine say ```/home/userName/python/webWatcher/```
-
-Your CSV takes:
-- Every row as a website to scrape.
-- Every row has four cells.
-- The first cell is the URL of the site to scrape.
-- The second cell is the kind of element to look for, eg, div or ul.
-- The third cell is the element identifier to look for, eg, the ID or the class. 
-- The fourth is the name of the element's class or ID.
-- Be careful not to have spaces in the columns if they aren't intended to be there, or BeautifulSoup can't find the element.
-- If you just want to scrape the whole page, enter null in the second and third cells for each row. Then the scraper will look for the html's body.
+You'll need to change the file locations when you call the _init_ function, to the same file path that Cron uses, where mine say ```/var/www/webWatcher/```
 
 Hope this script saves you some sanity.
 
 TO DO
-- Add a virtual env to emulate javascript for dynamic pages
 - Add a function to get the string that wasn't there in the older html, and tell the user what that is, so users don't have to follow links.
 - Add a function to push the results to an SQL database, and use Flask to create html templates from those results. So, users can choose to receive emails or check a website, or both.
--- WebWatcher.py
+++ WebWatcher.py
@@ -1,101 +1,243 @@
-#!/usr/bin/env python
-import requests, re, csv, sys, smtplib
+#!/usr/bin/python
+
+import re, csv, sys, smtplib, time, os, requests, httplib
 from bs4 import BeautifulSoup
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from datetime import datetime
+from contextlib import closing
+from xvfbwrapper import Xvfb
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException, TimeoutException
+vdisplay = Xvfb()
+vdisplay.start() #This is the function that begins the X server. It is required to run Spynner, which is a headless web browser.
 
-initTime = datetime.now()
+def format_web_page(filePathForCron, resultsForWebDisplay, initTime):
+    previousTime = ''
+    with open(filePathForCron + 'previousTime.txt', 'r+') as previousTxt:
+        previousTime = previousTxt.read()
+        previousTxt.seek(0)
+        previousTxt.truncate()
+        previousTxt.write(str(initTime))
+    pageHTML = []
+    pageHTML.append('<html><head>')
+    pageHTML.append('<link rel="stylesheet" href="style.css">')    
+    pageHTML.append('</head><body>')
+    previousScrapeString = '<h3>Previous scrape - %s' % (str(previousTime)[:16])
+    thisScrapeString = '<h3>This scrape - %s' % (str(initTime)[:16])    
+    pageHTML.append(previousScrapeString)
+    pageHTML.append(thisScrapeString)
+    pageHTML.append('<h3>Results</h3>')
+    pageHTML.append('<p>The <span class="changed">highlighted rows</span> are sites that have changed this time.</p>')
+    pageHTML.append('<p>The picture links on the <b>Screenshot</b> column, where available, show what the page looked like when it was saved the <b>previous time</b>.</p>')
+    pageHTML.append('<table>')
+    pageHTML.append('<tr><th>Site</th><th>Status</th><th>Screenshot</th></tr>')
+    for result in resultsForWebDisplay:
+        linkString = '<a href="%s" target="_blank">%s</a>' % (result[0], result[0])
+        if result[len(result)-2] != 'none':
+            picString = '<a href="%s" target="_blank">pic</a>' % (result[len(result)-2])
+        else:
+            picString = result[len(result)-2]
+        td1 = '<td>%s</td>' % (linkString)
+        td2 = '<td>%s</td>' % (result[len(result)-1])
+        td3 = '<td>%s</td>' % (picString)
+        if result[len(result)-1] == 'changed':
+            tr = '<tr class="changed">%s%s%s</tr>' % (td1,td2,td3)
+        else:
+            tr = '<tr>%s%s%s</tr>' % (td1,td2,td3)
+        pageHTML.append(tr)
+    pageHTML.append('<script></script>')
+    pageHTML.append('</table></body></html>')
+    pageHTML = ''.join(str(x) for x in pageHTML)
+    #Below, look for the file path entered in the init function, and use the filepath accordingly
+    with open(filePathForCron + 'index.html', 'w') as htmlFile:
+        htmlFile.write(pageHTML)
 
-def send_email(parseToEmail, toAddr, fromAddr, fromPswd):
-    #Send yourself the results. Need to setup an email account.
-    msg = MIMEMultipart()
-    msg['From'] = fromAddr
-    msg['To'] = toAddr
-    msg['Subject'] = 'Websites, update @ %s' %(str(initTime)[:16])  #Cut off the decimal places from the local time
-    msg.attach(MIMEText(parseToEmail, 'plain'))
-    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
-    server.starttls()
-    server.login(fromAddr, fromPswd)
-    text = msg.as_string()
-    server.sendmail(fromAddr, toAddr, text)
-    server.quit()
+def send_email(parseToEmail, peopleToEmail, fromAddr, fromPswd, initTime):
+    #Below, if you want emails about every scrape that occurs
+    #peopleToEmail.append('jason.thomas@sbs.com.au')
+    print 'peopleToEmail', peopleToEmail
+    for toAddr in peopleToEmail:
+        msg = MIMEMultipart()
+        msg['From'] = fromAddr
+        msg['To'] = toAddr
+        msg['Subject'] = 'Websites, update @ %s' %(str(initTime)[:16])  #Cut off the decimal places from the local time
+        msg.attach(MIMEText(parseToEmail, 'plain'))
+        server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+        server.starttls()
+        server.login(fromAddr, fromPswd)
+        text = msg.as_string()
+        server.sendmail(fromAddr, toAddr, text)
+        server.quit()
     return
 
-def format_email_text(websiteNotFound, pagesChanged, couldNotFind, contentMissing):
+def format_email_text(websiteNotFound, pagesChanged, couldNotFind, contentMissing, urlsSkipped, sitesInList, initTime, duplicates):
     if len(pagesChanged) > 0:
-        pagesChangedString = '\n'.join(pagesChanged)
+        pagesChangedString = '%s\n%s' %('\n'.join(pagesChanged),len(pagesChanged))
     else:
-        pagesChangedString = 'none'
+        pagesChangedString = '%s' % (len(pagesChanged))
     pagesChangedParse = 'These pages changed: \n%s\n' % (pagesChangedString)
+
+    websiteString = '<b>Go to %s' %('http://188.166.250.243/WebWatcher/ for a greater summary</b>')
+
     if len(websiteNotFound) > 0:    
-        websiteNotFoundString = '\n'.join(websiteNotFound)
+        websiteNotFoundString = '%s\n%s' %('\n'.join(websiteNotFound),len(websiteNotFound))
     else:
-        websiteNotFoundString = 'none'
-    websiteNotFoundParse = 'Could not be found online: \n%s\n' % (websiteNotFoundString)
+        websiteNotFoundString = '%s' % (len(websiteNotFound))
+    websiteNotFoundParse = 'Could not be found online/ request timed out: \n%s\n' % (websiteNotFoundString)
+
     if len(contentMissing) > 0:
-        contentMissingString = '\n'.join(contentMissing)
+        contentMissingString = '%s\n%s' %('\n'.join(contentMissing),len(contentMissing))
     else:
-        contentMissingString = 'none'        
+        contentMissingString = '%s' % (len(contentMissing))
     contentMissingParse = 'Could not find content: \n%s\n' % (contentMissingString)
+
     if len(couldNotFind) > 0:
-        couldNotFindString = '\n'.join(couldNotFind)
+        couldNotFindString = '%s\n%s' %('\n'.join(couldNotFind),len(couldNotFind))
     else:
-        couldNotFindString = 'none'    
+        couldNotFindString = '%s' % (len(couldNotFind))
     couldNotFindParse = 'Scraped and saved for the first time: \n%s\n' % (couldNotFindString)
+
+    otherString = '%s sites on list, %s skipped by instruction, %s duplicate urls also skipped \n' % (len(sitesInList), urlsSkipped, duplicates)
     finTime = datetime.now()
     timeTaken = finTime - initTime
-    timeString = 'The scraping took %s\n ~Started at %s and finished at %s' %(timeTaken, initTime, finTime)
-    return '%s\n\n%s%s%s\n%s' % (pagesChangedParse, websiteNotFoundParse, contentMissingParse, couldNotFindParse, timeString)
+    timeString = 'In total, took %s\n ~ Started at %s and finished at %s' %(timeTaken, initTime, finTime)
+    print 'timeString', timeString
+    return '%s\n\n%s\n\n%s%s%s\n%s%s' % (pagesChangedParse, websiteString, websiteNotFoundParse, contentMissingParse, couldNotFindParse, otherString, timeString)
+
+def PhantomJS_getshot_markup(filePathForCron, row, fileName):
+    try:
+        driver = webdriver.PhantomJS()
+        driver.set_window_size(2024, 3000)
+        driver.set_page_load_timeout(30)
+        driver.get(row[0])
+        time.sleep(float(row[7]))
+        newPhotoPath = '%s%s%s' % ('images/', fileName, 'new.png')
+        oldPhotoPath = '%s%s%s' % ('images/', fileName, 'old.png')    
+        if os.path.exists(filePathForCron + newPhotoPath):
+            os.rename(filePathForCron + newPhotoPath, filePathForCron + oldPhotoPath)
+        driver.save_screenshot(filePathForCron + newPhotoPath)
+        driver.quit()
+        print 'screenshot success'
+        #Returns old photo, since you don't want to look at the new one - the new one is the same as the webpage.
+        return oldPhotoPath
+    except httplib.BadStatusLine:
+        print '^ BadStatusLine (PhantomJS), screenshot fail'
+        return 'none'
+    except TimeoutException:
+        print '^ Timed out, screenshot fail'
+        return 'none'
+    except WebDriverException:
+        print '^ Typeerror, screenshot fail'
+        return 'none'
+
+def get_markup(row, fileName, websiteNotFound, contentMissing):
     
-def get_markup(row, websiteNotFound, contentMissing):
-    #Scrapes the page. If no such page, or if page has no content, this pushes errors to arrays for email
+    def requests_get_markup(row, fileName):
+        urlHeaders = {"User-Agent": "Jason Thomas, journalist, SBS News, https://github.com/JasonThomasData/WebWatcher, jason.thomas(at)sbs.com.au", "Referer": "www.sbs.com.au/news"}
+        r = requests.get(row[0], headers=urlHeaders, timeout=30)
+        return r.content
+
+    #Scrapes the page. If no such page, pushes that to an array
     print row[0]
     try:
-        #I recommend you enter your details here. If we want governments to be more open, than so should we be transparent.
-        urlHeaders = {"User-Agent": "YOUR_NAME, YOUR_EMAIL, https://github.com/JasonThomasData/WebWatcher, ", "Referer": "YOUR_SITE_HERE"}
-        r = requests.get(row[0], headers=urlHeaders, timeout=None)
+        scrapedHtml = requests_get_markup(row, fileName)
+        print 'request success'
     except IOError:
-        websiteNotFound.append(row[0])
-        print 'Site missing'
+        websiteNotFound.append(row[0]) #Integrate this into the above function
+        print '^ Site missing'
         return ''
-    try:
-        soup = BeautifulSoup(r.content, 'html5lib')
-        if row[2] == 'null' and row[3] == 'null':
-            findHtml = soup.find('body')
-            findHtml = str(findHtml.findAll(text=True))
-        else:
-            findHtml = soup.find(row[1], {row[2] : row[3]})
-            findHtml = str(findHtml.findAll(text=True))
-        #return findHtml.encode('ascii')
-        return re.sub('[\n\t\s\d\W]', '', findHtml)
     except AttributeError:
-        contentMissing.append(row[0])
-        print 'No content'
+        websiteNotFound.append(row[0])
+        print '^ Site missing'
         return ''
 
-def _init_(filePathForCron, fileWithURLs, toAddr, fromAddr, fromPswd):
-    #Start looping through the CSV, comparing and saving markup
+    #If page has no content, this pushes errors to an array for recording
+    try:
+        soup = BeautifulSoup(scrapedHtml, 'lxml')
+        if row[3] == 'null' and row[4] == 'null':
+            findHtml = soup.find('body')
+        else:
+            findHtml = soup.find(row[2], {row[3] : row[4]})
+        findHtml = str(findHtml.findAll(text=True))
+        return re.sub('[\n\t]', '', findHtml)
+    except AttributeError:
+        contentMissing.append(row[0])
+        print '^ No content'
+        return ''
+
+def _init_(filePathForCron, fileWithURLs, fromAddr, fromPswd):
+    initTime = datetime.now()
+    sitesInList = [] #Sites that have been processed
+    duplicates = 0
+    urlsSkipped = 0 #The CSV file will declare if a URL is to be skipped in row[5]
     pagesChanged = []
-    websiteNotFound = []
-    couldNotFind = []
-    contentMissing =[]
+    resultsForWebDisplay = []
+    websiteNotFound = [] #Or site timed out
+    couldNotFind = [] #Could not find file in savedFiles
+    contentMissing = [] #Could not find the element in the scraped markup          
+    peopleToEmail = []
+    
+    def add_email_to_list(emails):
+        emailList = emails.split(',')
+        for email in emailList:
+            if email not in peopleToEmail:
+                peopleToEmail.append(email)
+
+    def compare_save_html(row, fileName, externalHtml):
+        #Take screeshot, if instructed 
+        photoPath = 'none'
+        if row[8] == 'screenshot':
+            photoPath = PhantomJS_getshot_markup(filePathForCron, row, fileName)        
+        row.append(photoPath)
+
+        #Compare file with site
+        try:
+            with open(filePathForCron + 'savedFiles/' + fileName + '.txt', 'r') as internalHtml:
+                if externalHtml != internalHtml.read():
+                    pagesChanged.append(row[0]) #This gets sent to the email function
+                    compareResult = 'changed'
+                    add_email_to_list(row[1])
+                else:
+                    compareResult = 'unchanged'
+                row.append(compareResult)
+                resultsForWebDisplay.append(row) #Passed to the format_web_page() function   
+        except IOError:
+            couldNotFind.append(row[0])
+        with open(filePathForCron + 'savedFiles/' + fileName + '.txt', 'w') as fileToSave:
+            fileToSave.write(externalHtml)
+
+    #Start looping through the CSV, calling functions to compare and save markup
     with open(filePathForCron + fileWithURLs, 'r') as csvFile:
         csvRead = csv.reader(csvFile, delimiter=',')
         for row in csvRead:
-            externalHtml = get_markup(row, websiteNotFound, contentMissing)
-            fileName = re.sub(r'[^\w]', '', row[0])
-            fileName = fileName[:75] #Watch this, if you're scraping sites with the same URL, eg different q strings, you'll need to make this longer
-            try:
-                with open(filePathForCron + 'savedFiles/' + fileName + '.txt', 'r') as internalHtml:
-                    if externalHtml != internalHtml.read():
-                        pagesChanged.append(row[0])
-            except IOError:
-                couldNotFind.append(row[0])
-            with open(filePathForCron + 'savedFiles/' + fileName + '.txt', 'w') as fileToSave:
-                fileToSave.write(externalHtml)
-    parseToEmail = format_email_text(websiteNotFound, pagesChanged, couldNotFind, contentMissing)
-    send_email(parseToEmail, toAddr, fromAddr, fromPswd)
+            if row[0] not in sitesInList:  #Filters out duplicates
+                sitesInList.append(row[0])
+                if row[5] != 'skip': #Sites where the CSV contains an instruction to skip
+                    fileName = re.sub(r'[^\w]', '', row[0])
+                    fileName = fileName[:243] #THe maximum file length in Ubuntu is 255, so this is playing safe, including .txt, so this is safe by two characters
+                    externalHtml = get_markup(row, fileName, websiteNotFound, contentMissing)
+                    time.sleep(1) #Important for several requests from the same server, so you don't make their server slower (IP address could get blacklisted.
+                    if row[0] not in websiteNotFound and row[0] not in contentMissing:
+                        compare_save_html(row, fileName, externalHtml)
+                    else:
+                        print '^ File compare and save not attempted'
+                else:
+                    urlsSkipped += 1
+            else:
+                duplicates += 1
+    parseToEmail = format_email_text(websiteNotFound, pagesChanged, couldNotFind, contentMissing, urlsSkipped, sitesInList, initTime, duplicates)
+    send_email(parseToEmail, peopleToEmail, fromAddr, fromPswd, initTime)
+    format_web_page(filePathForCron, resultsForWebDisplay, initTime)
 
-_init_('/path/to/scraper/', 'websitesToScrape.csv', 'RECIPIENT_EMAIL_ADDRESS', 'SENDER_EMAIL_ADDRESS', 'SENDER_EMAIL_PASSPHRASE')
+vdisplay.stop()
+
+_init_('/var/www/html/WebWatcher/', 'websitesToScrape.csv', 'YOUR EMAIL HERE', 'YOUR EMAIL PASSWORD')
+#/var/www/html/WebWatcher/ - first param, path to folder required to run with cron
+#For testing, replace the first param with ''
+
+print 'Task finished at %s' %(datetime.now())
 sys.exit()
